b49169dfbf4f86e858dab1d4c94a41a90104cd24
Ainul Mardhiah <115910466+ainulmrd@users.noreply.github.com>
2022-10-22 23:10:06 +0800

    Update exercise_mall_customer.py

--- exercise_mall_customer.py
+++ exercise_mall_customer.py
@@ -5,20 +5,13 @@ import seaborn as sns
 import sklearn as sk
 import matplotlib.pyplot as plt
 
-st.header("My Second Streamlit App")
-st.write(pd.DataFrame({
-    'Intplan': ['yes', 'yes', 'yes', 'no'],
-    'Churn Status': [0, 0, 0, 1]
-}))
+st.header("Machine Learning for Mall Customer Data")
 
 mc = pd.read_csv('mall_customer.csv')
 
 mc.head()
-
 mc.tail()
-
 mc.describe()
-
 mc.info()
 
 x_mc = mc.drop(['Genre','CustomerID'], axis=1)  
@@ -31,21 +24,16 @@ from sklearn.model_selection import train_test_split
 Xtrain, Xtest, ytrain, ytest = train_test_split(x_mc, y_mc)
 Xtrain.head()
 
-Xtrain.shape #75%
-
-Xtest.shape #25%
-
 from sklearn.naive_bayes import GaussianNB
 model = GaussianNB()                       
 model.fit(Xtrain, ytrain)                 
 y_model = model.predict(Xtest)
 
 from sklearn.metrics import accuracy_score
-a = accuracy_score(ytest, y_model) #accuracy is low
+a = accuracy_score(ytest, y_model) 
 st.write("Accuracy score:", a)
 
 from sklearn.metrics import classification_report
-
 b = classification_report(ytest, y_model)
 st.write("Classification report:",b)
 
@@ -58,15 +46,15 @@ import matplotlib.pyplot as plt
 from sklearn import metrics
 import numpy as np
 confusion_matrix = metrics.confusion_matrix(ytest, y_model)
-
 c = confusion_matrix
 st.write("Confusion matrix:",c)
 
 cm_display = metrics.ConfusionMatrixDisplay(confusion_matrix = confusion_matrix,display_labels=np.unique(y_mc))
-
+fig = plt.figure(figsize=(10, 4))
 cm_display.plot()
 st.pyplot(fig)
 
+
 from sklearn.metrics import classification_report
 d = classification_report(ytest, y_model)
-st.write(d)
+st.write("Classification report:", d)
