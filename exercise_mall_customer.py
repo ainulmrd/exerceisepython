b8f7d7d9ab6e697d572fccfd6bded4cfc3e01403
Ainul Mardhiah <115910466+ainulmrd@users.noreply.github.com>
2022-10-22 23:48:43 +0800

    Update exercise_mall_customer.py

--- exercise_mall_customer.py
+++ exercise_mall_customer.py
@@ -37,7 +37,8 @@ st.write("Accuracy score:", a)
 from sklearn.metrics import classification_report
 b = classification_report(ytest, y_model)
 b.columns = ["Precision", "Recall", "F1-Score"]
-st.write("Classification report:",b)
+st.table(b)
+# st.write("Classification report:",b)
 
 # Confusion Matrix
 from sklearn.metrics import confusion_matrix 
