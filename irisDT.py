0c0c868b155058e8098bd8a20ebe58aceec540e9
Ainul Mardhiah <115910466+ainulmrd@users.noreply.github.com>
2022-10-23 13:23:56 +0800

    Update irisDT.py

--- irisDT.py
+++ irisDT.py
@@ -4,6 +4,9 @@ from sklearn.tree import plot_tree
 import pandas as pd
 from sklearn.model_selection import train_test_split
 import seaborn as sns
+from sklearn.tree import DecisionTreeClassifier
+from dtreeviz.trees import dtreeviz
+import graphviz as graphviz
 
 iris = sns.load_dataset('iris') 
 X_iris = iris.drop('species', axis=1)  
@@ -12,11 +15,18 @@ y_iris = iris['species']
 xtrain, xtest, ytrain, ytest = train_test_split(X_iris, y_iris,random_state=1)
 classf = tree.DecisionTreeClassifier()
 clsf = classf.fit(xtrain, ytrain)
+viz= dtreeviz(clsf, X_iris, y_iris, target_name="Classes",
+    feature_names=["f0", "f1"], class_names=["c0", "c1"])
+st.graphviz_chart(viz)
 
-st.write(clsf)
+# st.write(clsf)
 
 # fig = plt.figure(figsize=(10, 4))
-tree.plot_tree(classf.fit(xtrain, ytrain))
-st.pyplot(fig)
+# tree.plot_tree(classf.fit(xtrain, ytrain))
+# st.pyplot(fig)
 
 st.write(classf.score(xtest, ytest))
+
+
+# ------
+
