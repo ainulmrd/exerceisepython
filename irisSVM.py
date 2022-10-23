fff38fdbf91b5da1f49ce9adcbe5ca497fb4a5e1
Ainul Mardhiah <115910466+ainulmrd@users.noreply.github.com>
2022-10-23 10:51:33 +0800

    Update irisSVM.py

--- irisSVM.py
+++ irisSVM.py
@@ -10,7 +10,7 @@ y_iris = iris['species']
 xtrain, xtest, ytrain, ytest = train_test_split(X_iris, y_iris, random_state = 0)
 
 clf = SVC(kernel='rbf', C=1).fit(xtrain, ytrain)
-st.write('Iris dataset')
+# st.write('Iris dataset')
 st.write('Accuracy of RBF SVC classifier on training set: {:.2f}'
      .format(clf.score(xtrain, ytrain)))
 st.write('Accuracy of RBF SVC classifier on test set: {:.2f}'
