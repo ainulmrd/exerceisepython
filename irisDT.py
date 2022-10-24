2440288339fee938d57614f564f485f31a629aab
Ainul Mardhiah <115910466+ainulmrd@users.noreply.github.com>
2022-10-24 23:17:06 +0800

    Update irisDT.py

--- irisDT.py
+++ irisDT.py
@@ -1,20 +1,26 @@
-import streamlit as st
+import streamlit as st 
+import pandas as pd
+import seaborn as sns
+import pandas as pd
+import numpy as np 
+import matplotlib.pyplot as plt
+from sklearn.datasets import load_iris
 from sklearn import tree
 from sklearn.tree import plot_tree
-import pandas as pd
 from sklearn.model_selection import train_test_split
-import seaborn as sns
-from sklearn.tree import DecisionTreeClassifier
-from dtreeviz.trees import dtreeviz
-import graphviz as graphviz
 
-iris = sns.load_dataset('iris') 
+iris = sns.load_dataset('iris')
+
 X_iris = iris.drop('species', axis=1)  
 y_iris = iris['species']
 
-xtrain, xtest, ytrain, ytest = train_test_split(X_iris, y_iris,random_state=0)
-classf = tree.DecisionTreeClassifier()
-clsf = classf.fit(xtrain.data, ytrain.target)
-viz = dtreeviz(classf, xtrain.data, ytrain.target, target_name='species', feature_names = species.feature_names, class_names=["setosa", "versiolor","virginica"])
-st.graphviz_chart(viz)
-# viz.view() 
+xtrain, xtest, ytrain, ytest = train_test_split(X_iris, y_iris,random_state=1)
+clf = tree.DecisionTreeClassifier()
+clf = clf.fit(xtrain, ytrain)
+
+
+fig = plt.figure(figsize=(10, 4))
+clf.fit(xtrain, ytrain) 
+tree.plot_tree(clf.fit(xtrain, ytrain) )
+st.pyplot(fig)
+clf.score(xtest, ytest)
