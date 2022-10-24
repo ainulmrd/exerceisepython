3a585abeaa8e3eb007086ad886e5cbe21b6e68f6
Ainul Mardhiah <115910466+ainulmrd@users.noreply.github.com>
2022-10-24 23:19:33 +0800

    Update irisSVM.py

--- irisSVM.py
+++ irisSVM.py
@@ -18,15 +18,11 @@ y_iris = iris['species']
 xtrain, xtest, ytrain, ytest = train_test_split(X_iris, y_iris, random_state = 0)
 
 clf = SVC(kernel='rbf', C=1).fit(xtrain, ytrain)
-st.write('Iris dataset')
+st.write('SVM for Iris Dataset')
 st.write('Accuracy of RBF SVC classifier on training set: {:.2f}'
      .format(clf.score(xtrain, ytrain)))
 st.write('Accuracy of RBF SVC classifier on test set: {:.2f}'
      .format(clf.score(xtest, ytest)))
-
-X_iris.shape
-
-y_iris.shape
 
 model = SVC()                       
 model.fit(xtrain, ytrain)                  
@@ -36,7 +32,7 @@ a = accuracy_score(ytest, y_model)
 st.write("Accuracy score:", a)
 
 cr = classification_report(ytest, y_model)
-st.write(cr)
+st.write("Classification report:", cr)
 
 confusion_matrix(ytest, y_model)
 
