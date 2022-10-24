78ba521f57b5a7b427a8f75236d5be09b166608c
Ainul Mardhiah <115910466+ainulmrd@users.noreply.github.com>
2022-10-25 00:41:07 +0800

    Update irisKMEANS.py

--- irisKMEANS.py
+++ irisKMEANS.py
@@ -8,31 +8,25 @@ file = "iris.csv"
 
 iris = pd.read_csv(file)
 
-st.write("KMeans Clustering for Iris Dataset")
+st.header("KMeans Clustering for Iris Dataset")
 iris.head()
 
 features = ['sepal_length', 'sepal_width']
 iris_sepal = iris[features]
 
+# SEPAL
+st.write("Feature: Sepal")
 iris_sepal
 fig = plt.figure(figsize=(8, 4))
 plt.scatter(iris_sepal['sepal_length'], iris_sepal['sepal_width'], c = "brown");
 st.pyplot(fig)
 
-
 features = ['petal_length', 'petal_width']
 iris_petal = iris[features]
-
-iris_petal
-fig = plt.figure(figsize=(8, 4))
-plt.scatter(iris_petal['petal_length'], iris_petal['petal_width'], c = "salmon");
-st.pyplot(fig)
 
 kmeans = KMeans(n_clusters=4)
 sepal = kmeans.fit(iris_sepal)
 y1_kmeans = kmeans.predict(iris_sepal)
-petal = kmeans.fit(iris_petal)
-y2_kmeans = kmeans.predict(iris_petal)
 
 fig = plt.figure(figsize=(8, 4))
 plt.scatter(iris_sepal['sepal_length'], iris_sepal['sepal_width'], c=y1_kmeans, s=50, cmap='PRGn')
@@ -41,6 +35,16 @@ st.pyplot(fig)
 centers1 = kmeans.cluster_centers_
 st.write("Cluster Center (Sepal)")
 centers1
+
+# PETAL
+st.write("Feature: Petal")
+iris_petal
+fig = plt.figure(figsize=(8, 4))
+plt.scatter(iris_petal['petal_length'], iris_petal['petal_width'], c = "salmon");
+st.pyplot(fig)
+
+petal = kmeans.fit(iris_petal)
+y2_kmeans = kmeans.predict(iris_petal)
 
 fig = plt.figure(figsize=(8, 4))
 plt.scatter(iris_petal['petal_length'], iris_petal['petal_width'], c=y2_kmeans, s=50, cmap='inferno')
