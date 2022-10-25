71744ef9ac99aa2f4d263d46e0c99c5b4c2e09d3
Ainul Mardhiah <115910466+ainulmrd@users.noreply.github.com>
2022-10-25 13:42:47 +0800

    Update irisKMEANS.py

--- irisKMEANS.py
+++ irisKMEANS.py
@@ -14,7 +14,7 @@ mc.head()
 features = ['Annual_Income_(k$)', 'Spending_Score']
 mcF = mc[features]
 
-st.write("Scatter plot for Annual Income and Spending Score)
+st.write("Scatter plot for Annual Income and Spending Score")
 fig = plt.figure(figsize=(8, 4))
 plt.scatter(mcF['Annual_Income_(k$)'], mcF['Spending_Score'], c = "salmon");
 st.pyplot(fig)
@@ -23,7 +23,7 @@ kmeans = KMeans(n_clusters=4)
 kmeans.fit(mcF)
 y_kmeans = kmeans.predict(mcF)
 
-st.write("Scatter plot for Annual Income and Spending Score with centers)
+st.write("Scatter plot for Annual Income and Spending Score with centers")
 fig = plt.figure(figsize=(8, 4))         
 plt.scatter(mcF['Annual_Income_(k$)'], mcF['Spending_Score'], c=y_kmeans, s=50, cmap='inferno')
 centers = kmeans.cluster_centers_
